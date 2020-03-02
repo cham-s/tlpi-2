f67dc123daf180c7013c5a76596a877cb453eabc
cham-s <cc.attou@gmail.com>
2020-03-02 15:32:56 +0000

    tested duplicate and size of array

--- codes/proc/setenv-6-3/setenv.c
+++ codes/proc/setenv-6-3/setenv.c
@@ -1,19 +1,19 @@
 #include "tlpi_hdr.h"
 #include <string.h>
 
-static size_array(void **arary)
+static size_t size_array(char **array)
 {
-	size_t size;
+	size_t i;
 
-	size = 0;
+	i = 0;
 
-	while (array != NULL)
-		size += 1;
+	while (array[i] != NULL)
+		i += 1;
 
-	return size;
+	return i;
 }
 
-static Boolean is_there(consta char **array, const char *name)
+static Boolean is_there(char **array, char *name)
 {
 	int i;
 
@@ -27,9 +27,20 @@ static Boolean is_there(consta char **array, const char *name)
 	return FALSE;
 }
 
-static **duplicate
+static void print_array(char **array)
+{
+	size_t i;
 
-char **append_env(char	**env, const char *value)
+	i = 0;
+
+	while (array[i] != NULL)
+	{
+		printf("%s\n", array[i]);
+		i += 1;
+	}
+}
+
+char **append_env(char **env, char *value)
 {
 	int i;
 	size_t len;
@@ -41,32 +52,45 @@ char **append_env(char	**env, const char *value)
 	len_array = size_array(env);
 	i = 0;
 
-	new_array = (char **)malloc((izeof(char *) * (len_array + 1));
+	printf("size array\n");
+	new_array = (char **)malloc(sizeof(char *) * (len_array + 1));
 
 	if (new_array == NULL)
-		fatal("malloc failed at append_env() while setting new_array");
+		errExit("error malloc");
 
 	while (env[i] != NULL)
 	{
 		len = strlen(env[i]);
 		entry = (char *)malloc(sizeof(char)  * len);
-		strcpy(entry, env[i])
+		strcpy(entry, env[i]);
 
 		if (entry == NULL)
 			fatal("malloc failed at append_env() while setting entry");
 
 		new_array[i] = entry;
+		i += 1;
 	}
+	new_array[i] = value;
+	i++;
+	new_array[i] = NULL;
 
 	return new_array;
 }
 
-int m_setenv(const char *name, const char *value, int overwrite);
-{
 
-}
+/* int m_setenv(const char *name, const char *value, int overwrite);
+{
+} */
 
 int main(int argc, char *argv[], char **env)
 {
+	char **new_env;
+
+	printf("[DEBUG] env before:\n");
+	print_array(env);
+	new_env = append_env(env, "NEW=Value");
+	printf("[DEBUG] env after:\n");
+	print_array(new_env);
+
 	exit(EXIT_SUCCESS);
 }
