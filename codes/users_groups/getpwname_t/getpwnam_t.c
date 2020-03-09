e6f752c6efc1017c3ff5008d7f6420036ecf9db7
cham-s <cc.attou@gmail.com>
2020-03-09 18:39:04 +0000

    adding getpwnam

---
+++ codes/users_groups/getpwname_t/getpwnam_t.c
@@ -0,0 +1,15 @@
+#define _DEFAULT_SOURCE
+#define _XOPEN_SOURCE
+#include <unistd.h>
+#include <limits.h>
+#include <pwd.h>
+#include <shadow.h>
+#include "tlpi_hdr.h"
+
+int
+main(int argc, char *argv[])
+{
+	exit(EXIT_SUCCESS);
+}
+
+
